87e4e3ed4b37715a91a8fe6d26d7ccd3dcd51e6e
Cody Miller <cody.miller@learningfuze.com>
2020-09-09 14:47:18 -0700

    added warning that we are beginning to count

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function obviouslyADifferentNameThanCountUp(max){
+    console.log("Let's start counting!")
     while(true) {
         console.log('We\'ll get there someday');
         if(max > infinity) {
